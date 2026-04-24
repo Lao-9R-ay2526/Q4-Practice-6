b093e97e89b493969bff630bfa223d063dd2cada
Lao-9R-ay2526 <lao.lil@obmontessori.edu.ph>
2026-04-24 12:33:57 +0800

    Animal Counter

---
+++ main.js
@@ -0,0 +1,9 @@
+function noteAnimals() {
+    let anim = ["Lion", "Flamgo", "Polar Bear" , "Fish"];
+
+    for (let i = 0; anim.length; i++) {
+
+        window.alert("This zoo contains a " + animals[i] + "");
+    }
+
+}
